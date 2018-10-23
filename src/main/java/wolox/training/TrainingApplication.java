8d75192822dea202c9c9e5d0ffa32c907d881b7d
rodrigofranc <rodrigo.francou@wolox.com.ar>
2018-10-23 17:28:45 -0300

    Add Book model

--- src/main/java/wolox/training/TrainingApplication.java
+++ src/main/java/wolox/training/TrainingApplication.java
@@ -2,6 +2,9 @@ package wolox.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@EntityScan("org.wolox.training.models.Book")
 
 @SpringBootApplication
 public class TrainingApplication {
