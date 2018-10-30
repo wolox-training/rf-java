3218004dc54a770a0e43b08c815f3b26d9b974db
rodrigofranc <rodrigo.francou@wolox.com.ar>
2018-10-30 11:08:51 -0300

    Add more tests for controller Book

--- src/test/java/wolox/training/controllers/BookControllerTest.java
+++ src/test/java/wolox/training/controllers/BookControllerTest.java
@@ -1,8 +1,7 @@
 package wolox.training.controllers;
 
-import org.springframework.boot.test.context.TestConfiguration;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.web.WebAppConfiguration;
 import wolox.training.models.Book;
 import org.junit.jupiter.api.Test;
@@ -12,7 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import static org.mockito.BDDMockito.given;
 
 import static org.hamcrest.core.Is.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,7 +33,7 @@ class BookControllerTest {
     private MockMvc mvc;
 
     @MockBean
-    BookRepository service;
+    private BookRepository service;
 
     @Test
     public void findAll() throws Exception {
@@ -78,12 +77,64 @@ class BookControllerTest {
         List<Book> allBooks = Arrays.asList(book);
 
         given(service.findAll()).willReturn(allBooks);
-        String endpoind = "/api/books/" + book.getId();
-        mvc.perform(get(endpoind)
+        mvc.perform(get("/api/books/{id}", book.getId())
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content()
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("title", is(book.getTitle())));
+    }
+
+    @Test
+    public void create() throws Exception {
+        Book book = new Book();
+        book.setTitle("title test");
+        book.setGenre("genre test");
+        book.setAuthor("author test");
+        book.setImage("image test");
+        book.setIsbn("isbn test");
+        book.setPages(4321);
+        book.setSubtitle("subtitle test");
+        book.setPublisher("publisher test");
+        book.setYear("year test");
+
+        mvc.perform(post("/api/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(book)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void deleteOne() throws Exception {
+        mvc.perform(delete("/api/books", 1)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void updateOne() throws Exception {
+        Book book = new Book();
+        book.setTitle("title test");
+        book.setGenre("genre test");
+        book.setAuthor("author test");
+        book.setImage("image test");
+        book.setIsbn("isbn test");
+        book.setPages(4321);
+        book.setSubtitle("subtitle test");
+        book.setPublisher("publisher test");
+        book.setYear("year test");
+
+        mvc.perform(put("/api/books/{id}", book.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(book)))
+                .andExpect(status().isOk());
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
