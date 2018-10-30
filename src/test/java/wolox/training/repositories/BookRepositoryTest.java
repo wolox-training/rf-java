d8f5a9917d3993cb1736e32df49251736a7dd673
rodrigofranc <rodrigo.francou@wolox.com.ar>
2018-10-30 11:17:21 -0300

    Success tests cases

---
+++ src/test/java/wolox/training/repositories/BookRepositoryTest.java
@@ -0,0 +1,58 @@
+package wolox.training.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import wolox.training.models.Book;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void  findByAuthorSuccess() {
+        Book book = new Book();
+        book.setTitle("title test");
+        book.setGenre("genre test");
+        book.setAuthor("author test");
+        book.setImage("image test");
+        book.setIsbn("isbn test");
+        book.setPages(4321);
+        book.setSubtitle("subtitle test");
+        book.setPublisher("publisher test");
+        book.setYear("year test");
+
+        entityManager.persistAndFlush(book);
+        Book found = bookRepository.findByAuthor(book.getAuthor());
+        assertEquals(found.getAuthor(), book.getAuthor());
+    }
+
+    @Test
+    public void  createBookSuccess() {
+        Book book = new Book();
+        book.setTitle("title test");
+        book.setGenre("genre test");
+        book.setAuthor("author test");
+        book.setImage("image test");
+        book.setIsbn("isbn test");
+        book.setPages(4321);
+        book.setSubtitle("subtitle test");
+        book.setPublisher("publisher test");
+        book.setYear("year test");
+
+        entityManager.persistAndFlush(book);
+        Book found = bookRepository.getOne(book.getId());
+        assertNotNull(found);
+    }
+
+}
