8fcf918dc1d497ac37d6a99151645e49a13ffe23
rodrigofranc <rodrigo.francou@wolox.com.ar>
2018-11-05 11:12:35 -0300

    Create new resource for search books with query params

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.*;
 import wolox.training.models.Book;
 import wolox.training.repositories.BookRepository;
 import wolox.training.Exceptions.*;
+import wolox.training.services.OpenLibraryService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
@@ -15,6 +17,9 @@ public class BookController{
 
     @Autowired
     private BookRepository bookRepository;
+
+    @Autowired
+    private OpenLibraryService openLibraryService;
 
     @GetMapping
     public List<Book> findAll() {
@@ -48,6 +53,23 @@ public class BookController{
         bookRepository.findById(id)
                 .orElseThrow(BookNotFoundException::new);
         return bookRepository.save(book);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping("/search")
+    public Optional find(@RequestParam("isbn") String isbn) {
+        Book book = bookRepository.findByIsbn(isbn);
+        if(book == null) {
+            book = openLibraryService.find(isbn);
+            if(book == null) {
+                throw new BookNotFoundException();
+            } else {
+                book.setImage(""); // image is required for save the book
+                bookRepository.save(book);
+            }
+
+        }
+        return Optional.of(book);
     }
 
 }
--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -8,4 +8,5 @@ import wolox.training.models.Book;
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     Book findByAuthor(String author);
+    Book findByIsbn(String isbn);
 }
