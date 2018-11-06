e79043d084c828f0d989cb4b02112225f9939c3f
rodrigofranc <rodrigo.francou@wolox.com.ar>
2018-11-06 12:27:36 -0300

    Add correct annotation to user repository

--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -1,8 +1,10 @@
 package wolox.training.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import wolox.training.models.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
