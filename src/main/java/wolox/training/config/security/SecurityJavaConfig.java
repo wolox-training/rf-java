40a981e03823cd1fe2dae0bd1bad3fd12ca3be77
rodrigofranc <rodrigo.francou@wolox.com.ar>
2018-11-07 15:00:36 -0300

    Disable csrf

--- src/main/java/wolox/training/config/security/SecurityJavaConfig.java
+++ src/main/java/wolox/training/config/security/SecurityJavaConfig.java
@@ -3,6 +3,7 @@ package wolox.training.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,12 +17,20 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
     private CustomAuthenticationProvider authProvider;
 
     @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable().httpBasic()
+                .and()
+                .authorizeRequests().anyRequest().authenticated();
+    }
+
+    @Override
     public void configure(WebSecurity web) {
         web
-            .ignoring()
-            .antMatchers(HttpMethod.POST, "/api/books")
-            .and()
-            .ignoring().antMatchers(HttpMethod.POST, "/api/Users");
+                .ignoring()
+                .antMatchers(HttpMethod.POST, "/api/books")
+                .and()
+                .ignoring().antMatchers(HttpMethod.POST, "/api/Users");
     }
 
     @Override
