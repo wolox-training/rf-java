2c30d6467022883c7521c07a157c1bf17a6a9efb
rodrigofranc <rodrigo.francou@wolox.com.ar>
2018-11-13 12:54:48 -0300

    Add required annotations for repositories

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -3,10 +3,8 @@ package wolox.training.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
 import wolox.training.models.Book;
 
-@Repository
 public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
     Book findByAuthor(String author);
     Book findByIsbn(String isbn);
--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -3,12 +3,10 @@ package wolox.training.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
 import wolox.training.models.User;
 
 import java.time.LocalDate;
 
-@Repository
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
     User findByUsername(String username);
